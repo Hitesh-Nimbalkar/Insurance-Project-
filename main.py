6faf28b2eec4762194e3b9f394ec7bfb5d5fff3f
Hitesh Nimbalkar <nimbalkarhitesh@gmail.com>
2023-02-18 21:51:14 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from Insurance_Project.entity.config_entity import DataIngestionConfig
 from Insurance_Project.entity import config_entity
 from Insurance_Project.components.data_ingestion import DataIngestion
 from Insurance_Project.components.data_validation import DataValidation
+from Insurance_Project.components.data_transformation import DataTransformation
 
 
 
@@ -29,6 +30,12 @@ if __name__=="__main__":
                          data_ingestion_artifact=data_ingestion_artifact)
         
        data_validation_artifact = data_validation.initiate_data_validation()
+
+      # Data transformation
+       data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+       data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+       data_ingestion_artifact=data_ingestion_artifact)
+       data_transformation_artifact = data_transformation.initiate_data_transformation()
 
       
      except Exception as e:
