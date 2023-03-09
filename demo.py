77fe8bf323e43ecdd11a1b906e6c76726f6967c5
Hitesh <nimbalkarhitesh@gmail.com>
2023-03-09 20:38:19 +0530

    Batch prediction demo complete

---
+++ demo.py
@@ -0,0 +1,12 @@
+from Insurance_Project.pipeline.batch_prediction import start_batch_prediction
+from Insurance_Project.pipeline.training_pipeline import start_training_pipeline
+
+file_path=r"/config/workspace/insurance.csv"
+print(__name__)
+if __name__=="__main__":
+    try:
+        #output_file = start_training_pipeline()
+        output_file = start_batch_prediction(input_file_path=file_path)
+        print(output_file)
+    except Exception as e:
+        print(e)
